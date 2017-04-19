bdbe742851d6ba385d16b35a0012f34130319c68
unknown <chu.byron@yahoo.com>
2017-04-19 22:30:38 +1000

    used process.arg to access command line arguments

--- app.js
+++ app.js
@@ -5,29 +5,19 @@ const _= require('lodash');  // lodash module
 
 const notes= require('./notes')
 
+// argument from command line
+var command = process.argv[2]
+console.log('command:',command);
 
 
-// trying out lodash
-// console.log(_.isString(true));
-// console.log(_.isString('Byron'))
-
-var filteredArray= _.uniq(['byron',1,2,3,'byron',4,3]);
-console.log(filteredArray);
-
-// using required function from notes.js file
-// var res = notes.addNote();
-// console.log(res);
-//
-// var  added = notes.add( 9, -2)
-// console.log(added);
-
-
-
-// var user = os.userInfo();
-// console.log(user)
-//
-// fs.appendFile('greetings.txt', `Hello ${user.username}! `, function(err){
-//   if (err) {
-//     console.log('unable to write to file');
-//   }
-// });
+if (command === 'add') {
+  console.log('adding a note');
+} else if (command === 'list') {
+    console.log('listing a note');
+} else if (command === 'remove') {
+    console.log('remove a note');
+} else if (command === 'read') {
+    console.log('reading a note');
+} else {
+  console.log('command not recognised');
+};
